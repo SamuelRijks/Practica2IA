23c052c3a430b440c181244c1498035dd49f0825
Samur18 <105276323+Samur18@users.noreply.github.com>
2024-05-31 23:14:55 +0200

    removed classes

--- order.py
+++
@@ -1,9 +0,0 @@
-class Order:
-    def __init__(self, order_id, weight, restaurant, delivery_coords):
-        self.order_id = order_id
-        self.weight = weight
-        self.restaurant = restaurant
-        self.delivery_coords = delivery_coords
-
-    def __repr__(self):
-        return f"Order({self.order_id}, {self.weight}kg, {self.restaurant}, {self.delivery_coords})"
